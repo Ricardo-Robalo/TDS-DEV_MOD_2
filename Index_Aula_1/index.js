e86173efc8ccfe96092b350a4506efd523b19efe
Catioro_Baiano <robalodacruzr@gmail.com>
2024-09-11 22:00:42 -0300

    Subindo coisas do melhor dia do ano

--- Index_Aula_1/index.js
+++ Index_Aula_1/index.js
@@ -1,16 +1,51 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
- //Cai ali 
-// http://localhost:8080/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Código do /
 app.get("/",(request, response) => {
 
     response.status(200).send("Teu server n ta de brinqueixons");
 });
 
-app.listen(8080, () => {
-
-    console.log("Hello Potato");
+//Código do /novarota
+app.get("/novarota", (request, response) => {
+    
+    response.send("Nova rota, fodase");
 });
 
+//Código do /clienteS
+app.get("/clientes", (request, response) => {
+    
+    const clientes = [
+    {
+
+        nome: "Robalto",
+        idade: 20
+    },
+    {
+        nome: "Bagre",
+        idade: 17
+    }
+]
+
+response.send(clientes);
+});
+
+//Código do /cliente
+app.post("/cliente", (request, response) => {
+
+    const cliente = request.body;
+
+    response.send(cliente);
+});
+
+
+app.listen(8080, () => {
+
+    console.log("Começou a putaria");
+});
