540410ccbf40c790a069e36b6c89650ba4cb3649
Catioro_Baiano <robalodacruzr@gmail.com>
2024-11-06 20:02:33 -0300

    Subindo essa bosta pra tentar não reprovar nessa merda

--- API_Tema_Livre/controller/usuario.controller.js
+++ API_Tema_Livre/controller/usuario.controller.js
@@ -6,30 +6,46 @@ cadastro: (req, res) => {
 
     const {nome, nick, email} = req.body;
 
-        if (!nome) {
+        var comando = ``;
+
+        if (nome) {
+
+            comando = `INSERT INTO CLIENTE(nome) VALUES('${nome}',null)`
+            
+        } else {
+
             return res.status(309).send({ msg: "O nome é obrigatorio no cadastro de jogador!" });
         }
 
-        var comando = ``;
+        if (nick) {
 
-        if (!nick) {
-            comando = `INSERT INTO CLIENTE(nome, 
-            nick) VALUES('${nome}',null)`
+            comando = `INSERT INTO CLIENTE(nick) VALUES('${nick}',null)`
+
         } else {
-            comando = `INSERT INTO CLIENTE(nome, 
-            nick) VALUES('${nome}','${nick}')`
+
+            return res.status(309).send({ msg: "O nick é obrigatorio no cadastro de jogador!" });
+        }
+
+        if (email) {
+
+            comando = `INSERT INTO CLIENTE(email) VALUES('${email}',null)`
+            
+        } else {
+
+            return res.status(309).send({ msg: "O nome é obrigatorio no cadastro de jogador!" });
         }
 
         conn.raw(comando)
             .then((data) => {
-                res.status(200).send({ msg: "Cliente cadastrado com sucesso!" });
+                res.status(200).send({ msg: "Usuário cadastrado com sucesso!" });
             })
             .catch((error) => {
-                res.status(500).send("Erro ao cadastrar um cliente!");
+                res.status(500).send("Erro ao cadastrar o usuário!");
             });
 },
 atualizar: (req, res) => {
 
+    
 },
 consultar: (req, res) => {
 
